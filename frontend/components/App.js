b35cc79650b686aacee9b4fc47d52a59b0033001
babotavo419 <one.faith419@gmail.com>
2023-06-07 21:28:45 -0400

    todo.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default class App extends React.Component {
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
-        Todo App
+      <h1>Todos</h1>
+
+
+      <ul>
+        {
+          todos.map(todo=> {
+            return (<Todo todo={todo}/>)
+          })
+        }
+      </ul>
+
+      <form>
+        <input/>
+        <button>Add</button>
+      </form>
+
+      <button>Clear</button>
       </div>
-    )
+    );
   }
 }
+
+export default App
