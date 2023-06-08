eef967659025ed76df1b0122985993a84c8b214f
babotavo419 <one.faith419@gmail.com>
2023-06-08 18:06:47 -0400

    ok

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,30 +5,59 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          name: 'Organize Garage',
-          id: 1528817077286, // could look different, you could use a timestamp to generate it
-          completed: false
-        },
-        {
-          name: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ]
+      todos: []
     }
   }
+
+  handleAdd = (task) => {
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+
+
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  handleToggle = (clickedId) => {
+  this.setState({
+    ...this.state,
+    todos: this.state.todos.map(todo=> {
+      if (todo.id === clickedId) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      } 
+        return todo
+   
+    })
+  });
+  }
+
   render() {
     const { todos } = this.state;
 
     return (
       <div>
       <h1>Todos</h1>
-
-      <TodoList todos={todos} />
-      <TodoForm />
-      <button>Clear</button>
+      <TodoList handleToggle={this.handleToggle} todos={todos} />
+      <TodoForm handleAdd={this.handleAdd}/>
+      <button onClick={this.handleClear}>Clear</button>
       </div>
     );
   }
